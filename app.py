7c365822017abbb1826718e4aa283977b2c50ab8
sapphacs13 <sappha.omeara@gmail.com>
2015-05-05 11:47:39 -0400

    stuff

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ app.secret_key = "hello"
 
 @app.route('/', methods=['GET'])
 def index():
-    return None
+    #render_template("index.html")
+    return "Home Page"
 
 @app.route('/comments', methods=['GET'])
 def get_comments():
-
     return [{'url':"555.555.co.uk",'comment':"Fake Real Comment"}
             ,{'url':"hello.world.com",'comment':"Fake Comment"}
             ,{'url':"www.why.org",'comment':"Not Real Comment"}
             ,{'url':"www.dummy.net",'comment':"Test Comment"}]
+
 @app.route('/comments/new', methods=['POST'])
 def store_comments():
-    return None
-
+    return "stuff"
 
 
 #All Boring Names that Justin gives me and no Fun Benedict Names makes Ben a dull boy
